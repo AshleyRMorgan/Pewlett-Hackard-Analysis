761ae4f50bd9e9a282a8821dfa8c162a1a74cfa2
Avanderschuere <ashleyrae1219@gmail.com>
2022-05-23 19:04:07 -0700

    module partially completed

---
+++ Queries/practice_queries.sql
@@ -0,0 +1,30 @@
+--Module 7 practice
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
